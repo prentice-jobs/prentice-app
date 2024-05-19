355f0a22bc84d21e98da5ec5e6bda539fe4c1af0
Rahmaniatok <93527684+Rahmaniatok@users.noreply.github.com>
2024-05-19 19:35:22 +0700

    Add Navbar

--- app/src/main/java/com/prenticedev/prenticeapp/ui/MainActivity.kt
+++ app/src/main/java/com/prenticedev/prenticeapp/ui/MainActivity.kt
@@ -1,13 +1,20 @@
-package com.prenticedev.prenticeapp
+package com.prenticedev.prenticeapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.prenticedev.prenticeapp.R
 import com.prenticedev.prenticeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +27,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_foryou, R.id.navigation_explore, R.id.navigation_compare
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
 }
