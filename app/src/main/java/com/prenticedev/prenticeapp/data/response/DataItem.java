38de44d112f9e61c0cc9030d1c9abf8605e61e5b
Rahmaniatok <93527684+Rahmaniatok@users.noreply.github.com>
2024-05-22 19:20:47 +0700

    Feed - On Progress

---
+++ app/src/main/java/com/prenticedev/prenticeapp/data/response/DataItem.java
@@ -0,0 +1,55 @@
+package com.prenticedev.prenticeapp.data.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DataItem{
+
+	@SerializedName("role")
+	private String role;
+
+	@SerializedName("reviewDate")
+	private String reviewDate;
+
+	@SerializedName("reviewContent")
+	private String reviewContent;
+
+	@SerializedName("companyName")
+	private String companyName;
+
+	@SerializedName("location")
+	private String location;
+
+	@SerializedName("type")
+	private String type;
+
+	@SerializedName("reviewTitle")
+	private String reviewTitle;
+
+	public String getRole(){
+		return role;
+	}
+
+	public String getReviewDate(){
+		return reviewDate;
+	}
+
+	public String getReviewContent(){
+		return reviewContent;
+	}
+
+	public String getCompanyName(){
+		return companyName;
+	}
+
+	public String getLocation(){
+		return location;
+	}
+
+	public String getType(){
+		return type;
+	}
+
+	public String getReviewTitle(){
+		return reviewTitle;
+	}
+}
